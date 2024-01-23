06d994b4c03fef588bfcdc012d4b9e5422f2a448
DIegoDevcode <jdiegomoreno05@gmail.com>
2024-01-23 17:17:10 -0500

    Añadiendo html y los estilos css

---
+++ scrip.js
@@ -0,0 +1,6 @@
+// Variables con el método querySelector la logica de los botones
+let addBtn = document.querySelector('.btn');
+let todoInput = document.querySelector('#todo-input');
+let todoList = document.querySelector('todo-list');
+
+addBtn.addEventListener()
